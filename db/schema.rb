8b4869d87e045daa501a370c12614e9e33448173
EDDIVIII <pietroeddispina@gmail.com>
2019-03-07 22:54:26 +0100

    autorizzazioni e associazioni e associazioni per user e contents

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_06_092451) do
+ActiveRecord::Schema.define(version: 2019_03_07_111449) do
 
   create_table "contents", force: :cascade do |t|
     t.string "titolo"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_03_06_092451) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
